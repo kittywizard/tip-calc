6c4a6bb6014f00ab9a641eaa30a52168b5ec5e96
marleigh <volcana@gmail.com>
2022-07-08 17:10:05 -0700

    context is BROKENNNN

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Header from "./components/header/Header";
 import {StyledApp} from "./styles/container";
 import Form from "./components/form/Form";
 import Results from "./components/results/Results"
-
-interface AppState {
-  displayResults: boolean
-}
+import { Context } from "./Context";
 
 const App:React.FC = () => {
 
-  const [displayResults, setDisplayResults] = useState<AppState["displayResults"]>(false);
-
+  //const {displayResults, setDisplayResults} = useContext(Context);
+  const [displayResults, setDisplayResults] = useState(false);
+  
   return (
-    <StyledApp>
-      <Header/>
-      {
-        !displayResults ?
-        <Form 
-          setDisplayResults={setDisplayResults}
-        /> :
-        <div>Success</div>
-      }
-   </StyledApp>
+        <StyledApp>
+              <Header/>
+              {
+                !displayResults ?
+                <Form 
+                  setDisplayResults={setDisplayResults}
+                /> :
+                <div>Success</div>
+              }
+          </StyledApp>    
   )
 }
 
---
+++ src/Context.tsx
@@ -0,0 +1,30 @@
+import { Context, createContext, useState } from "react";
+
+interface ContextState {
+    displayResults: boolean,
+    setDisplayResults?: (prevState: boolean) => boolean,
+}
+
+const defaultState = {
+    displayResults: false
+}
+
+const Context = createContext<ContextState>(defaultState);
+
+
+interface AppState {
+    displayResults: boolean
+  }
+
+const ContextProvider:React.FC = ({children}) => {
+
+    const [displayResults, setDisplayResults] = useState<AppState["displayResults"]>(false);
+
+    return (
+        <Context.Provider value={{displayResults, setDisplayResults}}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {Context, ContextProvider}
