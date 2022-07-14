4f98c1663e2f4805266c3a1fb5a45110dc31bdc0
marleigh <volcana@gmail.com>
2022-07-13 17:25:21 -0700

    colors

--- src/components/input/input-styles.ts
+++ src/components/input/input-styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const StyledInput = styled.input`
-    border: 1px solid #12ffcc;
-    background-color: #eee;
+    border: 2px solid #12ffcc;
+    background-color: #fff;
     font-family: "Gantari", sans-serif;
     padding:  0.25em 1em;
     border-radius: 0.5em;
